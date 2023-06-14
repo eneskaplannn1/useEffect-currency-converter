41ed15afd22a1a19b1ef1526039d7b469a6993ff
eneso-kaplannn1 <muhammedenes.kaplan@std.yeditepe.edu.tr>
2023-06-14 17:13:02 +0300

    initial commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,48 @@
-import logo from './logo.svg';
-import './App.css';
+// `https://api.frankfurter.app/latest?amount=100&from=EUR&to=USD`
 
-function App() {
+import { useEffect, useState } from "react";
+
+export default function App() {
+  const [amount, setAmount] = useState();
+  const [convertedAmount, setConvertedAmount] = useState(0);
+  const [from, setFrom] = useState("USD");
+  const [to, setTo] = useState("EUR");
+  console.log(from, to);
+
+  useEffect(() => {
+    if (!amount) return;
+    async function Converter() {
+      const res = await fetch(
+        `https://api.frankfurter.app/latest?amount=${amount}&from=${from}&to=${to} `
+      );
+      const data = await res.json();
+      console.log(data);
+      setConvertedAmount(data.rates[to]);
+    }
+    if (from === to) return setConvertedAmount(amount);
+    Converter();
+  }, [amount, from, to]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div>
+      <input
+        type="number"
+        value={amount}
+        onChange={(e) => {
+          setAmount(e.target.value);
+        }}
+      />
+      <select value={from} onChange={(e) => setFrom(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+      </select>
+      <select value={to} onChange={(e) => setTo(e.target.value)}>
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="INR">INR</option>
+      </select>
+      <p>{convertedAmount ? ` OUTPUT ${convertedAmount} ${to}` : ""}</p>
     </div>
   );
 }
-
-export default App;
